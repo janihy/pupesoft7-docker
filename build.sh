0d9961bf52d9d024ac53e38ebe2fb8d2a3c8ac74
Jani Hyytiäinen <jani.hyytiainen@iki.fi>
2025-02-22 18:35:28 +0200

    use docker instead of podman, works out of the box in proxmox containers

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+apt update && apt install -y git docker docker-compose
 git clone https://github.com/akirjavainen/pupesoft7.git --depth=1
-podman build -t pupesoft7 .
+docker build -t pupesoft7 .
+docker-compose up -d
