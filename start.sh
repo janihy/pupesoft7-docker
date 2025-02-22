8cb3bfd54b28c221883754db415c5f9669b6e36f
Jani Hyytiäinen <jani.hyytiainen@iki.fi>
2025-02-22 18:43:02 +0200

    echo default ip address on start

--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@ apt update && apt install -y git docker docker-compose
 git clone https://github.com/akirjavainen/pupesoft7.git --depth=1
 docker build -t pupesoft7 .
 docker-compose up -d
+echo "Pupesoft should now be accessible at http://$(hostname -I | awk '{print $1}'):8080"
